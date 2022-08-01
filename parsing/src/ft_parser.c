6329313e3b89b0d81c2230726965fdec3c4fb6bd
Mohammed Ed-dobaji <med-doba@e3r9p11.1337.ma>
2022-08-01 19:15:48 +0100

    handle pipe and quotes

--- parsing/src/ft_parser.c
+++ parsing/src/ft_parser.c
@@ -6,26 +6,44 @@
 /*   By: med-doba <med-doba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 10:06:38 by med-doba          #+#    #+#             */
-/*   Updated: 2022/07/31 18:56:06 by med-doba         ###   ########.fr       */
+/*   Updated: 2022/08/01 19:13:21 by med-doba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../mini.h"
 
-void	ft_parser(t_lexer *lexer, char *rtn)
+void	ft_parser(t_lexer *lexer, char *str)
 {
-	t_lexer	*tmp;
+	int	i;
+	int	j;
+	int	yes;
+	i = 0;
+	j = 0;
+	yes = 1;
+	i = ft_skip_withespace(str, i);
+	while (str[i])
+	{
+		if (str[i] == '"' || str[i] == '\'')
+		{
+			while (str[i] == '"')
+			{
+				i++;
+				while (str[i] != '"')
+				{
+					i++;
+				}
+				if (str[i + 1] == '"')
+					i++;
 
-	tmp = lexer;
-	if (ft_locate_char(rtn, '\'') % 2 != 0 || ft_locate_char(rtn, '"') % 2 != 0)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	while (tmp)
-	{
-		printf("parser = %s\n", tmp->content);
-		tmp = tmp->next;
+			}
+			while(str[i] == '\'')
+			{}
+		}
+		if (str[i] == '|')
+		{}
+		if (str[i] == '<' || str[i] == '>')
+		{}
+		i++;
 	}
 }
 
@@ -82,58 +100,82 @@ int	ft_check_stock(char *str)
 	return (0);
 }
 
-// int	ft_check_pipe(char *str)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (str[i])
-// 	{
-// 		if ()
-// 	}
-// }
-int ft_ft(char *str)
-{
-	int	i;
-	int	j;
-	int	yes;
-	i = 0;
-	j = 0;
-	yes = 1;
-	i = ft_skip_withespace(str, i);
-	while (str[i])
-	{
-		if (str[i] == '"' || str[i] == '\'')
-		{
-			while (str[i] == '"')
-			{
-				i++;
-				while (str[i] != '"')
-				{
-					i++;
-				}
-				if (str[i + 1] == '"')
-					i++;
-
-			}
-			while(str[i] == '\'')
-			{}
-		}
-		if (str[i] == '|')
-		{}
-		if (str[i] == '<' || str[i] == '>')
-		{}
-		i++;
-	}
-}
-
-char	*ft_skip_quotes(t_lexer **lexer, char *str, char c, int i)
+char	*ft_scan_pipe(char *str, char c, int *i)
 {
 	char	*rtn;
+	char	*tmp;
 
-	rtn = 
-	while (str[i] != c)
+	rtn = ft_strdup("");
+	if (str[(*i) + 1] == c || str[(*i) + 1] == '\0')
 	{
-		i++;
+		printf("error_pipe\n");
+		return (NULL);
 	}
+	tmp = ft_char_to_str(str[*i]);
+	rtn = ft_strjoin(rtn, tmp);
+	free(tmp);
+	return (rtn);
+}
+
+char	*ft_scan_redirection(char *rtn, int *i, char c)
+{
+	char	*rtn;
+	char	*tmp;
+	int		count;
+
+	count = 0;
+	rtn = ft_strdup("");
+	while (rtn[*i] == c)
+	{
+		if (count == 3 || rtn[*i] == '\0')
+		{
+			free(rtn);
+			printf("error_redirection\n");
+			return (NULL);
+		}
+		tmp = ft_char_to_str(rtn[*i]);
+		rtn = ft_strjoin(rtn, tmp);
+		free(tmp);
+		(*i)++;
+		count++;
+	}
+	return (rtn);
+}
+
+char	*ft_scan_quotes(t_lexer **lexer, char *str, char c, int *i)
+{
+	char	*rtn;
+	char	*tmp;
+
+	rtn = ft_strdup("");
+	while (1)
+	{
+		if (str[*i] == c)
+		{
+			*i++;
+			if (str[*i] && (str[*i] == '"' || str[*i] == '\'' || str[*i] != ' ' || str[*i] != '\t'))
+			{
+				if (str[*i] == '"')
+					rtn = ft_strjoin(rtn, ft_skip_quotes(lexer, str, c, ++(*i)));
+				else if (str[*i] == '\'')
+					rtn = ft_strjoin(rtn, ft_skip_quotes(lexer, str, '\'', ++(*i)));
+			}
+			else if (str[*i] && str[*i] == ' ' || str[*i] == '\t')
+				break ;
+			if (str[*i] == '\0')
+				return (rtn);
+		}
+		if (str[*i] == '\0')
+		{
+			free(rtn);
+			return (NULL);
+		}
+		// if (str[*i] == ' ' || str[*i] == '\t')
+		// 	break;
+		tmp = ft_char_to_str(str[*i]);
+		rtn = ft_strjoin(rtn, tmp);
+		free(tmp);
+		*i++;
+	}
+	return (rtn);
 }
