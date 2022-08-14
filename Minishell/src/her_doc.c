d75681ee52a7aeb953f484d16e07a4d7a4ceb1bb
Abderrahmane Daifi <adaifi@e2r1p1.1337.ma>
2022-08-14 10:55:59 +0100

    Heredoc is done >> pipe under construction

--- Minishell/src/her_doc.c
+++ Minishell/src/her_doc.c
@@ -6,7 +6,7 @@
 /*   By: adaifi <adaifi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 17:42:09 by adaifi            #+#    #+#             */
-/*   Updated: 2022/08/14 10:49:37 by adaifi           ###   ########.fr       */
+/*   Updated: 2022/08/14 10:55:19 by adaifi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ int	her_doc(t_lexer *arg)
 
 	tmp = open("tmp", O_RDWR | O_CREAT | O_TRUNC, 00777);
 	s = get_next_line(0);
-	write(tmp, s, ft_strlen(s));
 	arg->content = ft_strjoin(arg->content, "\n");
+	if (s == NULL || !ft_strcmp(s, arg->content))
+		return (tmp);
+	write(tmp, s, ft_strlen(s));
 	while (ft_strcmp(s, arg->content))
 	{
 		s = get_next_line(0);
